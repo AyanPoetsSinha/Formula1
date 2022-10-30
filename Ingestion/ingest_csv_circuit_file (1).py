bd06b602a34bab38aedf7625af7932253814f998
ayan.india.2009@gmail.com <ayan.india.2009@gmail.com>
2022-10-30 21:32:06 +0000

    setup and mounting

---
+++ Ingestion/ingest_csv_circuit_file (1).py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ###Ingest csv file
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### step1 : read the file with spark dataframe reader
+
+# COMMAND ----------
+
+df = spark.read.csv('')
+
+# COMMAND ----------
+
+dbutils.fs.mounts()
+
+# COMMAND ----------
+
+
---
+++ setup/mountadls_storage (1).py
@@ -0,0 +1,64 @@
+# Databricks notebook source
+dbutils.secrets.help()
+
+# COMMAND ----------
+
+dbutils.secrets.list("formula1scope")
+
+# COMMAND ----------
+
+dbutils.secrets.get(scope='formula1scope', key="databricks-app-client-id")
+
+# COMMAND ----------
+
+storage_account_name="formula1ayansa0001"
+client_id=dbutils.secrets.get(scope='formula1scope', key="databricks-app-client-id")
+tenant_id=dbutils.secrets.get(scope='formula1scope', key="databricks-app-tenant-id")
+client_secret=dbutils.secrets.get(scope='formula1scope', key="databricks-app-client-secret")
+
+
+# COMMAND ----------
+
+configs = {"fs.azure.account.auth.type": "OAuth",
+          "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+          "fs.azure.account.oauth2.client.id": f"{client_id}",
+          "fs.azure.account.oauth2.client.secret": f"{client_secret}",
+          "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"}
+
+
+
+# COMMAND ----------
+
+container_name = "raw"
+dbutils.fs.mount(
+  source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
+  mount_point = f"/mnt/{storage_account_name}/{container_name}",
+  extra_configs = configs)
+
+# COMMAND ----------
+
+dbutils.fs.ls("mnt/formula1ayansa0001/raw")
+
+# COMMAND ----------
+
+dbutils.fs.mounts()
+
+# COMMAND ----------
+
+def mount_adls(container_name):
+    dbutils.fs.mount(
+    source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
+    mount_point = f"/mnt/{storage_account_name}/{container_name}",
+    extra_configs = configs)
+
+# COMMAND ----------
+
+mount_adls("raw")
+
+# COMMAND ----------
+
+mount_adls("processed")
+
+# COMMAND ----------
+
+
