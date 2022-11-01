12c281ff48d679ee93ee25929f89bc735f5b7e0f
ayan.india.2009@gmail.com <ayan.india.2009@gmail.com>
2022-11-01 13:58:08 +0000

    1/11 early push

--- Ingestion/ingest_csv_circuit_file (1).py
+++ Ingestion/ingest_csv_circuit_file (1).py
@@ -9,12 +9,80 @@
 
 # COMMAND ----------
 
-df = spark.read.csv('')
+circuits_df = spark.read \
+.option("header", True) \
+.option("inferSchema", True) \
+.csv("dbfs:/mnt/formula1ayansa0001/raw/circuits.csv")
 
 # COMMAND ----------
 
-dbutils.fs.mounts()
+dbutils.fs.mounts()  
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ##look through the file system of the path
 
+# COMMAND ----------
+
+display(circuits_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###describing the schema
+
+# COMMAND ----------
+
+circuits_df.printSchema()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###checking the min max value to determine the correct datatype
+
+# COMMAND ----------
+
+circuits_df.describe().show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###manually setiing up the attribute datatype
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType,StructField,IntegerType,StringType,DoubleType
+
+# COMMAND ----------
+
+circuits_df.printSchema()
+
+# COMMAND ----------
+
+circuits_schema=StructType(fields=[StructField("circuitId",IntegerType(),False),
+                                   StructField("circuitRef",StringType(),False),
+                                   StructField("name",StringType(),False),
+                                   StructField("location",StringType(),False),
+                                   StructField("country",StringType(),False),
+                                   StructField("lat",DoubleType(),False),
+                                   StructField("lng",DoubleType(),False),
+                                   StructField("alt",IntegerType(),False),
+                                   StructField("url",StringType(),False)
+                             ])
+
+# COMMAND ----------
+
+circuit_df = spark.read \
+.option("header", True) \
+.schema(circuits_schema) \
+.csv("dbfs:/mnt/formula1ayansa0001/raw/circuits.csv")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Select the important Columns
+
+# COMMAND ----------
+
+circuits
