e236278227f0bedccd11efd4e6dbe3cf5e973845
ayan.india.2009@gmail.com <ayan.india.2009@gmail.com>
2022-11-04 23:29:29 +0000

    5_11_qualifying and lap times push late night

---
+++ Ingestion/ingest_csv_Lap_times (1).py
@@ -0,0 +1,74 @@
+# Databricks notebook source
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###ingest lap times multiple files
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###read using dataframe reader api
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType,StructField,StringType,IntegerType
+
+
+# COMMAND ----------
+
+lap_times_schema= StructType(fields=[StructField("raceId",IntegerType(),True),
+                                  StructField("driverId",IntegerType(),True),
+                                  StructField("position",IntegerType(),True),
+                                  StructField("lap",IntegerType(),True),
+                                  StructField("time",StringType(),True),
+                                  StructField("miliseconds",IntegerType(),True)])
+
+# COMMAND ----------
+
+lap_times_df=spark.read \
+.schema(lap_times_schema) \
+.csv("/mnt/ayanstorage0001/raw/lap_times/*.csv")
+
+# COMMAND ----------
+
+display(lap_times_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###rename columns and add new columns
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
+lap_times_final_df=lap_times_df \
+.withColumnRenamed("raceId","race_id") \
+.withColumnRenamed("driverId","driver_id") \
+.withColumn("ingestion_time",current_timestamp())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### write the processed in output in  processed parquet format
+
+# COMMAND ----------
+
+lap_times_final_df.write.mode("overwrite").parquet("/mnt/ayanstorage0001/processed/lap_times")
+
+# COMMAND ----------
+
+df=spark.read.parquet("/mnt/ayanstorage0001/processed/lap_times")
+
+# COMMAND ----------
+
+display(df)
---
+++ Ingestion/ingest_json_multi_line_qualifying.py
@@ -0,0 +1,80 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ###ingest qualifying multi line json data
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###read using dataframe reader api
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType,StructField,StringType,IntegerType,DoubleType
+
+
+# COMMAND ----------
+
+qualifying_schema= StructType(fields=[StructField("qualifyId",IntegerType(),True),
+                                  StructField("raceId",IntegerType(),True),
+                                  StructField("driverId",IntegerType(),True),
+                                  StructField("constructorId",IntegerType(),True),
+                                  StructField("number",IntegerType(),True),
+                                  StructField("position",IntegerType(),True),
+                                  StructField("q1",StringType(),True),
+                                  StructField("q2",StringType(),True),
+                                  StructField("q3",StringType(),True)])
+
+# COMMAND ----------
+
+qualifying_df=spark.read \
+.schema(qualifying_schema) \
+.option("multiline", True) \
+.json("/mnt/ayanstorage0001/raw/qualifying")
+
+# COMMAND ----------
+
+display(qualifying_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###rename columns and add new columns
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+qualifying_final_df=qualifying_df \
+.withColumnRenamed("raceId","race_id") \
+.withColumnRenamed("driverId","driver_id") \
+.withColumnRenamed("qualifyId","qualify_id") \
+.withColumnRenamed("constructorId","constructor_id") \
+.withColumn("ingestion_time",current_timestamp())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### write the processed in output in  processed parquet format
+
+# COMMAND ----------
+
+qualifying_final_df.write.mode("overwrite").parquet("/mnt/ayanstorage0001/processed/qualifying")
+
+# COMMAND ----------
+
+df=spark.read.parquet("/mnt/ayanstorage0001/processed/qualifying")
+
+# COMMAND ----------
+
+display(df)
