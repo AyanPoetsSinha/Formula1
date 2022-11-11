0719399b0dcb15ae4bf75ae2180a97c60fcce472
ayan.india.2009@gmail.com <ayan.india.2009@gmail.com>
2022-11-11 08:12:16 +0000

    11_11_1:42 push window function

--- demo/demo.py
+++ demo/demo.py
@@ -13,10 +13,124 @@ display(df)
 
 # COMMAND ----------
 
-df \
-.groupBy("driver_name") \
+demo_df=df.filter("race_year='2021'")
+
+# COMMAND ----------
+
+display(demo_df
+       )
+
+# COMMAND ----------
+
+from pyspark.sql.functions import count,countDistinct,sum
+
+# COMMAND ----------
+
+demo_df.select(count("*")).show()
+
+# COMMAND ----------
+
+demo_df.select(count("race_name")).show()
+
+# COMMAND ----------
+
+demo_df.select(countDistinct("race_name")).show()
+
+# COMMAND ----------
+
+demo_df.select(sum("points")).show()
+
+# COMMAND ----------
+
+demo_df.filter("driver_name= 'Lewis Hamilton'").select(sum("points"), countDistinct("race_name")) \
+.withColumnRenamed("sum(points)","total points") \
+.withColumnRenamed("count(DISTINCT race_name)","Total Races") \
 .show()
 
 # COMMAND ----------
 
-f
+# MAGIC %md
+# MAGIC ### Group Aggregations
+
+# COMMAND ----------
+
+demo_df \
+.groupBy("driver_name") \
+.sum("points").show()
+
+# COMMAND ----------
+
+demo_df \
+.groupBy("driver_name") \
+.agg(sum("points"),countDistinct ("race_name")) \
+.show()
+
+# COMMAND ----------
+
+demo_df \
+.groupBy("driver_name") \
+.agg(sum("points"),countDistinct ("race_name")) \
+.orderBy("sum(points)", ascending=False) \
+.show()
+
+# COMMAND ----------
+
+demo_df \
+.groupBy("driver_name") \
+.agg(sum("points").alias("Total Points"),countDistinct ("race_name").alias("Total Races")) \
+.orderBy("Total Points", ascending=False) \
+.show()
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ### window funcitons
+
+# COMMAND ----------
+
+demo_df=df.filter("race_year in ('2020','2021')")
+
+# COMMAND ----------
+
+display(demo_df)
+
+# COMMAND ----------
+
+demo_df \
+.groupBy("race_year","driver_name") \
+.agg(sum("points").alias("Total Points"),countDistinct ("race_name").alias("Total Races")) \
+.orderBy("Total Points", ascending=False) \
+.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### ranking
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### importing
+
+# COMMAND ----------
+
+from pyspark.sql.window import WIndow
+from pyspark.sql.functions import desc
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###
+# MAGIC creating specificaiton
+
+# COMMAND ----------
+
+driverRankSpec=Window.partitionBy("race_year").orderBy(desc(total_points))
+
+# COMMAND ----------
+
+demo_df.withColumn("rank", rank().over(driverRankSpec)).show()
+
+# COMMAND ----------
+
+
