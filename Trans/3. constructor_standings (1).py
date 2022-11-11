57eb35cfd6e76a5cd77934b2eb6deb9a893c691f
ayan.india.2009@gmail.com <ayan.india.2009@gmail.com>
2022-11-11 11:28:54 +0000

    11_11_4_57 PUSH

--- Trans/3. constructor_standings (1).py
+++ Trans/3. constructor_standings (1).py
@@ -22,22 +22,22 @@ from pyspark.sql.functions import sum, when,count,col,desc,asc
 
 from pyspark.sql.functions import sum, when,count,col,desc,asc
 
-driver_standing_df=race_results_df \
-.groupBy("race_year", "driver_name", "driver_nationality", "team") \
+constructor_standing_df=race_results_df \
+.groupBy("race_year", "team") \
 .agg(sum("points").alias("total_points"),
     count(when(col("position")==1,True)).alias("Wins"))
 
 # COMMAND ----------
 
-display(driver_standing_df)
+display(constructor_standing_df)
 
 # COMMAND ----------
 
 from pyspark.sql.window import Window
 from pyspark.sql.functions import rank, desc, asc
 
-driver_rank_spec=Window.partitionBy("race_year").orderBy(desc("total_points"),asc("Wins"))
-final_df = driver_standing_df.withColumn("rank",rank().over(driver_rank_spec))
+constructor_rank_spec=Window.partitionBy("race_year").orderBy(desc("total_points"),asc("Wins"))
+final_df = constructor_standing_df.withColumn("rank",rank().over(constructor_rank_spec))
 
 
 # COMMAND ----------
@@ -46,7 +46,7 @@ display(final_df)
 
 # COMMAND ----------
 
-final_df.write.mode("overwrite").parquet(f"{presentation_folder_path}/driver_standings")
+final_df.write.mode("overwrite").parquet(f"{presentation_folder_path}/constructor_standings")
 
 # COMMAND ----------
 
